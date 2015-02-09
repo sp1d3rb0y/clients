790bd60ebe9e157d2447957e8c12512e643d2f10
sp1d3rb0y <sp1d3rb0y@gmail.com>
2015-02-09 01:21:14 +0100

    Added Slack History Retriever client

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # clients
 Various clients for various APIs (Freebox, Subsonic, Slack, ...)
+
+[SLACK]
+The slack client retrieves history for all Slack channels and writes it in a file.
+There is one file per channel.
+Edit the script to add your Slack Token (can be found @ https://api.slack.com/web) and modify the path where the logs are written.
+10000 messages roughly take ~30M of disk space.
---
+++ slack-history-retriever.py
@@ -0,0 +1,104 @@
+## slack.py for Slack History Retriever in ~/slack-history
+##
+## Made by spiderboy
+##
+## Started on  Sun Feb  8 18:07:32 2015 spiderboy
+## Last update Sun Feb  8 23:20:58 2015 spiderboy
+##
+#!/usr/bin/python
+
+import requests
+import json
+import re
+import io
+
+log_path = "~/slack-history/"
+# Find your token @ https://api.slack.com/web
+token = ""
+
+def get_method_result(method, args = ""):
+    url = "https://lescoupains.slack.com/api/"
+    r = requests.get("%s%s?token=%s&%s" % (url, method, token, args))
+    res = json.loads(r.text)
+    return res
+
+def get_channels():
+    chans = {}
+    res = get_method_result("channels.list")
+    for chan in res['channels']:
+        chans[chan['id']] = chan['name']
+    return chans
+
+def get_users():
+    users = {}
+    res = get_method_result("users.list")
+    for user in res['members']:
+        users[user["id"]] = user['name']
+        users[user['name']] = user['id']
+    return users
+
+def replace_chans_and_users(line, users, chans):
+    users_to_replace = re.findall('<@?(U[A-Z0-9]{8})>', line)
+    for u in users_to_replace:
+        if u in users:
+            line = re.sub(u,users[u],line)
+    chans_to_replace = re.findall('<#?(C[A-Z0-9]{8})>', line)
+    for c in chans_to_replace:
+        if c in chans:
+            line = re.sub(c, chans[c], line)
+    return line
+
+def get_latest(channel):
+    try:
+        f = open('%s/.%s.latest' % (log_path, channel),'r')
+        res = f.readlines()
+        f.close()
+        return res[0].replace("\n",'')
+    except Exception,e:
+        return None
+
+def set_latest(channel, latest):
+    f = open("%s/.%s.latest" % (log_path, channel), "w")
+    f.write(latest)
+    f.close()
+
+def write_log(channel, lines):
+    f = io.open("%s/%s.log" % (log_path, channel), "a", encoding='utf8')
+    for l in lines:
+        f.write(l + '\n')
+    f.close()
+
+def get_channel_history(id_channel, channel_name):
+    args = "count=1000&channel=%s" % id_channel
+    latest = get_latest(channel_name)
+    if latest is not None:
+        args += "&oldest=%s" % latest
+    res = get_method_result("channels.history", args)
+    hasmore = res['has_more']
+    while hasmore:
+        args = "count=1000&channel=%s" % id_channel
+        args += "&latest=%s" % res['messages'][0]['ts']
+        newres = get_method_result("channels.history", args)
+        res['messages'] = newres['messages'] + res['messages']
+        hasmore = newres['has_more']
+    formatted = []
+    if 'messages' in res and res['messages']:
+        set_latest(channel_name, res['messages'][0]['ts'])
+        for message in reversed(res['messages']):
+            if 'user' in message:
+                formatted.append("<%s> %s" % (message['user'], message['text']))
+            else:
+                formatted.append("%s" % (message['text']))
+        return formatted
+    else:
+        return ""
+
+channels = get_channels()
+users = get_users()
+
+for chan in channels:
+    lines = get_channel_history(chan, channels[chan])
+    logs = []
+    for l in lines:
+        logs.append(replace_chans_and_users(l, users, channels))
+    write_log(channels[chan], logs)
